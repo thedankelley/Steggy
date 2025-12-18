94087ec0b8c77915c9c3aabaf00da26387337f73
thedankelley <113657070+thedankelley@users.noreply.github.com>
2025-12-18 10:52:29 -0500

    Delete sstv directory

--- sstv/sstv.js
+++
@@ -1,8 +0,0 @@
-/*
-  SSTV Emergency Transport Module
-  Experimental and observable
-*/
-
-export function imageToSSTVWave(imageData) {
-  throw new Error("SSTV encoding is experimental and disabled by default.");
-}
