9b4efae697ffa0b7cc8f6ca60326717d8ad799c3
thedankelley <113657070+thedankelley@users.noreply.github.com>
2025-12-18 10:38:46 -0500

    Update steggy-pgp.js

--- modules/steggy-pgp.js
+++ modules/steggy-pgp.js
@@ -1,29 +1,31 @@
-import * as openpgp from "https://cdn.jsdelivr.net/npm/openpgp@5.10.0/+esm";
+import * as openpgp from "https://cdn.jsdelivr.net/npm/openpgp@5.10.1/+esm";
 
-export async function generatePGPKeyPair() {
-  return openpgp.generateKey({
-    type:"rsa",
-    rsaBits:4096,
-    userIDs:[{name:"Steggy User"}],
-    passphrase:""
+export async function generatePGPKeyPair(passphrase) {
+  const { privateKey, publicKey } = await openpgp.generateKey({
+    type: "rsa",
+    rsaBits: 4096,
+    userIDs: [{ name: "Steggy User" }],
+    passphrase
   });
+
+  return { publicKey, privateKey };
 }
 
 export async function pgpEncrypt(text, publicKeyArmored) {
-  const publicKey = await openpgp.readKey({armoredKey:publicKeyArmored});
-  const message = await openpgp.createMessage({text});
-  return openpgp.encrypt({message,encryptionKeys:publicKey});
+  const key = await openpgp.readKey({ armoredKey: publicKeyArmored });
+  return openpgp.encrypt({
+    message: await openpgp.createMessage({ text }),
+    encryptionKeys: key
+  });
 }
 
 export async function pgpDecrypt(cipher, privateKeyArmored, passphrase) {
-  const privateKey = await openpgp.decryptKey({
-    privateKey:await openpgp.readPrivateKey({armoredKey:privateKeyArmored}),
-    passphrase
-  });
-  const message = await openpgp.readMessage({armoredMessage:cipher});
+  const key = await openpgp.readPrivateKey({ armoredKey: privateKeyArmored });
+  const unlocked = await openpgp.decryptKey({ privateKey: key, passphrase });
+  const msg = await openpgp.readMessage({ armoredMessage: cipher });
   const { data } = await openpgp.decrypt({
-    message,
-    decryptionKeys:privateKey
+    message: msg,
+    decryptionKeys: unlocked
   });
   return data;
 }
