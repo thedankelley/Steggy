0fde9a870afe612d0c15a8ebdb350898790ffbf5
thedankelley <113657070+thedankelley@users.noreply.github.com>
2025-12-20 10:05:46 -0500

    Update steggy-hash.js

--- modules/steggy-hash.js
+++ modules/steggy-hash.js
@@ -1,4 +1,11 @@
-export async function sha256(buffer) {
+export async function sha256(data) {
+  const buffer =
+    typeof data === "string"
+      ? new TextEncoder().encode(data)
+      : data;
+
   const hash = await crypto.subtle.digest("SHA-256", buffer);
-  return [...new Uint8Array(hash)].map(b => b.toString(16).padStart(2, "0")).join("");
+  return [...new Uint8Array(hash)]
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
 }
