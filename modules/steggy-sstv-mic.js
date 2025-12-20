b8228796d6647e26d2eaca3c8bb318e10e570958
thedankelley <113657070+thedankelley@users.noreply.github.com>
2025-12-20 10:08:32 -0500

    Update steggy-sstv-mic.js

--- modules/steggy-sstv-mic.js
+++ modules/steggy-sstv-mic.js
@@ -1,28 +1,14 @@
-// Live microphone SSTV capture
-let audioCtx;
-let processor;
-let stream;
-let buffer = [];
-
-export async function startMicCapture(onData) {
-  audioCtx = new AudioContext();
-  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
+export async function listenAndDecode(callback) {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const audioCtx = new AudioContext();
   const source = audioCtx.createMediaStreamSource(stream);
-  processor = audioCtx.createScriptProcessor(4096, 1, 1);
 
+  const processor = audioCtx.createScriptProcessor(4096, 1, 1);
   source.connect(processor);
   processor.connect(audioCtx.destination);
 
-  processor.onaudioprocess = e => {
-    const input = e.inputBuffer.getChannelData(0);
-    buffer.push(...input);
-    onData(buffer, audioCtx.sampleRate);
+  processor.onaudioprocess = event => {
+    const input = event.inputBuffer.getChannelData(0);
+    callback(input);
   };
-}
-
-export function stopMicCapture() {
-  if (processor) processor.disconnect();
-  if (stream) stream.getTracks().forEach(t => t.stop());
-  buffer = [];
 }
